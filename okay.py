a18a50419bee0e1132686bfa510b903214756eb1
WoshoForegun <tsuchekage@gmail.com>
2021-08-27 19:22:04 +0300

    bad program

---
+++ okay.py
@@ -0,0 +1,108 @@
+import arcade
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Drawing Example")
+arcade.set_background_color(arcade.color.WHITE)
+arcade.start_render()
+x = 300
+y = 300
+radius = 200
+arcade.draw_circle_filled(x, y, radius, arcade.color.YELLOW)
+x = 370
+y = 300
+radius = 200
+arcade.draw_circle_filled(x, y, radius, arcade.color.BLACK)
+x = 230
+y = 350
+radius = 20
+arcade.draw_circle_filled(x, y, radius, arcade.color.BLACK)
+x = 300
+y = 280
+width = 120
+height = 100
+start_angle = 190
+end_angle = 350
+arcade.draw_arc_outline(x, y, width, height, arcade.color.BLACK, start_angle, end_angle, 10)
+arcade.finish_render()
+arcade.run()
+def draw_pine_tree(x, y):
+    arcade.draw_triangle_filled(x + 40, y, x, y - 100, x + 80, y - 100, arcade.color.DARK_GREEN)
+    arcade.draw_lrtb_rectangle_filled(x + 30, x + 50, y - 100, y - 140, arcade.color.DARK_BROWN)
+    SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+class MyGame(arcade.Window):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        arcade.set_background_color(arcade.color.AMAZON)
+        def setup(self):
+            pass
+        def on_draw(self):
+            arcade.start_render()
+            def update(self, delta_time):
+                 pass
+def main():
+    game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT)
+    game.setup()
+arcade.run()
+if __name__ == "__main__":
+    main() 
+    SPRITE_SCALING_COIN = 0.2
+    coin = arcade.Sprite("coin_01.png", SPRITE_SCALING_COIN)
+    def setup(self):
+        self.player_list = arcade.SpriteList()
+self.coin_list = arcade.SpriteList()
+self.score = 0
+self.player_sprite = arcade.Sprite("images/character.png", SPRITE_SCALING_PLAYER)
+self.player_sprite.center_x = 50
+self.player_sprite.center_y = 50
+self.player_list.append(self.player_sprite)
+for i in range(COIN_COUNT):
+    coin = arcade.Sprite("images/coin_01.png", SPRITE_SCALING_COIN)
+    coin.center_x = random.randrange(SCREEN_WIDTH)
+    coin.center_y = random.randrange(SCREEN_HEIGHT)
+    self.coin_list.append(coin)
+    def on_draw(self):
+        arcade.start_render()
+        self.coin_list.draw()
+        self.player_list.draw()
+def update(self, delta_time):
+    coins_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.coin_list)
+    for coin in coins_hit_list:
+        coin.kill()
+        self.score += 1
+        self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite, self.wall_list)
+        MOVEMENT_SPEED = 5
+        if key == arcade.key.UP:
+            self.player_sprite.change_y = MOVEMENT_SPEED
+        elif key == arcade.key.DOWN:
+                self.player_sprite.change_y = -MOVEMENT_SPEED
+        elif key == arcade.key.LEFT:
+            self.player_sprite.change_x = -MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT:
+            self.player_sprite.change_x = MOVEMENT_SPEED
+            def on_key_release(self, key, modifiers):
+                            if key == arcade.key.UP or key == arcade.key.DOWN:
+                                self.player_sprite.change_y = 0
+        elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
+            self.player_sprite.change_x = 0
+            def update(self, delta_time):
+                self.physics_engine.update()
+                self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite, self.wall_list, gravity_constant=GRAVITY)
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                          
+
+        
